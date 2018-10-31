aeeae840fc49615b1bc293b61602f52e850ce188
Adithya Visnu <adithyavisnu@gmail.com>
2018-10-31 12:37:53 +0900

    token refresh

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ function User(email, departement="", employment="user") {
     this.email = email;
     this.departement = departement;
     this.employment = employment;
-    this.token = "jwtTokenProvided";
+    this.token = "generatedJWToken";
+    this.refresh = "generatedRefreshToken";
 }
 
 User.prototype.get = function get() {
     return this;
-}
-
-User.prototype.verify = function verifyUser(token, departement, employment) {
-    return true;
 }
 
 module.exports = User
